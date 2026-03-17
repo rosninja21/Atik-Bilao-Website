19619f956e5220436838e5404e30da76a816740d
rosninja21 <ryuichinina@gmail.com>
2026-03-17 18:28:04 +0800

    organized files

--- js/script.js
+++ js/script.js
@@ -39,96 +39,96 @@ const dishes=[{
     name:"Fried Chicken",
     desc: "Crispy, golden-brown fried chicken perfect for sharing.",
     prices: {small: 300, medium: 400, large: 600},
-    image: "images/friedchicken.png",
-    category: "main"
+    image: "../images/friedchicken.png",
+    category: "../main"
 }, {name: "Pancit Bam-I",
     desc: "A Cebuano noodle dish featuring a combination of pancit canton and sotanghon.",
     prices: {small: 300, medium: 400, large: 600},
-    image: "images/pancit-bam-e.png",
+    image: "../images/pancit-bam-e.png",
     category: "main"
 }, {
     name: "Pancit Sotanghon",
     desc: "Stir-fried cellophane noodles with meat and vegetables.",
     prices: {small: 300, medium: 450, large: 650},
-    image: "images/pancit-sotanghon.png",
+    image: "../images/pancit-sotanghon.png",
     category: "main"
 },{
     name: "Pork Lollipop",
     desc: "Breaded and fried pork lollipops, a favorite for kids and adults alike.",
     prices: {medium: 400, large: 600},
-    image: "images/pork-lollipop.png",
+    image: "../images/pork-lollipop.png",
     category: "main"
 }, {
     name: "Buttered Shrimp",
     desc: "Juicy shrimp cooked in rich butter and garlic sauce.",
     prices: {medium: 400, large:600},
-    image: "images/buttered-shrimp.png",
+    image: "../images/buttered-shrimp.png",
     category: "main"
 }, {
     name: "Pork Laroca",
     desc: "Savory pork dish cooked to perfection.",
     prices: {small: 300, medium: 400, large: 600},
-    image: "images/laroca-img.png",
+    image: "../images/laroca-img.png",
     category: "main"
 
 },{
     name: "Fish Fillet",
     desc: "Breaded fish fillet, crispy on the outside and tender on the inside.",
     prices: {medium: 400, large:600},
-    image: "images/fish-fillet.png",
+    image: "../images/fish-fillet.png",
     category: "main"
 },{
     name: "Cordon Bleu",
     desc: "Breaded meat wrapped around cheese and ham, fried to perfection.",
     prices: {small: 300 , medium: 400, large:600},
-    image: "images/cordon-bleu.png",
+    image: "../images/cordon-bleu.png",
     category: "main"
 }, {
 
      name: "Chicken Fillet",
     desc: "Crispy fried chicken fillet bites.",
     prices: {small: 300 , medium: 400, large:600},
-    image: "images/chicken-fillet.png",
+    image: "../images/chicken-fillet.png",
     category: "main"
 },{
      name: "Lumpiang Shanghai",
     desc: "Filipino-style spring rolls with ground pork and vegetables.",
     prices: {small: 300 , medium: 400, large:600},
-    image: "images/lumpiang-shanghai.png",
+    image: "../images/lumpiang-shanghai.png",
     category: "main"
 
 },  {
      name: "Pork Humba",
     desc: "Braised pork belly with a sweet and savory sauce.",
     prices: {small: 300 , medium: 400, large:600},
-    image: "images/pork-humba.png",
+    image: "../images/pork-humba.png",
     category: "main"
 
 }, {
     name: "Chicken Fillet",
     desc: "Fried spring rolls filled with fresh vegetables.",
     prices: {small: 300 , medium: 400, large:600},
-    image: "images/chicken-fillet.png",
+    image: "../images/chicken-fillet.png",
     category: "main"
 },{
     name: "Pork Meatballs (Bola-bola)",
     desc: "Delicious fried pork meatballs.",
     prices: {small: 300 , medium: 400, large:600},
-    image: "images/bola-img.png",
+    image: "../images/bola-img.png",
     category: "main"
 
 },{
      name: "Spaghetti",
     desc: "Sweet-style Filipino spaghetti with hotdogs and ground meat.",
     prices: {medium: 300, large:500},
-    image: "images/spaghetti.png",
+    image: "../images/spaghetti.png",
     category: "main"
 
 },{
      name: "Chopsuey",
     desc: "Stir-fried mixed vegetables with meat or seafood.",
     prices: {medium: 400, large:600},
-    image: "images/chapsuey-img.png",
+    image: "../images/chapsuey-img.png",
     category: "main"
 
 
@@ -136,69 +136,69 @@ const dishes=[{
     name: "3 in 1 Combo",
     desc: "Selection includes Laroca, Pancit, Cordon Bleu, Meatballs, Chicken Fillet, Vegetable Lumpia, Lumpiang Shanghai, or Fried Chicken.",
     contents: ["Laroca", "Pancit", "Cordon Bleu", "Meatballs", "Chicken Fillet","Vegetable Lumpia", "Lumpiang Shanghai", "Fried Chicken"],
-    image: "images/3-in1-combo.png",
+    image: "../images/3-in1-combo.png",
     category: "combos"
 
 }, {name: "2 in 1 Combo",
     desc: "Selection includes Laroca, Pancit, Cordon Bleu, Meatballs, or Vegetable Lumpia.",
     contents:["Laroca", "Pancit", "Cordon Bleu", "Meatballs", "Vegetable Lumpia"],
-    image:"images/2-in-1-combo.png",
+    image:"../images/2-in-1-combo.png",
     category: "combos"
 
 },{
     name: "Puto Cheese",
     desc:"Soft steamed rice cakes topped with cheese.",
     prices:{piece: 5},
-    image: "images/puto-cheese.png",
+    image: "../images/puto-cheese.png",
     category: "desserts"
 },{
      name: "Pichi-Pichi ",
     desc:"Sticky cassava dessert coated in grated coconut or cheese.",
     prices:{piece: 5},
-    image: "images/pichi.png",
+    image: "../images/pichi.png",
     category: "desserts"
 },{
      name: "Coconut Macaroons ",
     desc:"Sweet and chewy coconut treats.",
     prices:{pieces: 300},
-    image: "images/macaroons.png",
+    image: "../images/macaroons.png",
     category: "desserts"
 
 },{
      name: "Ube Jam ",
     desc:"Rich and creamy purple yam jam.",
     prices:{tub: 100},
-    image: "images/ube-jam.png",
+    image: "../images/ube-jam.png",
     category: "desserts"
     
 },{ name: "Maja Blanca ",
     desc:"Coconut milk pudding with corn kernels.",
     prices:{tub: 100},
-    image: "images/maja.png",
+    image: "../images/maja.png",
     category: "desserts"
     },{
         name: "Mango Float ",
     desc:"Layers of graham crackers, cream, and fresh mangoes.",
     prices:{tub: 100},
-    image: "images/mango-float.png",
+    image: "../images/mango-float.png",
     category: "desserts"
     },{
         name: "Torta ",
     desc:"Traditional sponge cake, perfect for coffee or tea.",
     prices:{small: 230, large: 280},
-    image: "images/torta.png",
+    image: "../images/torta.png",
     category: "desserts"
     },{
         name: "Biko ",
     desc:"Sweet sticky rice cake topped with latik (coconut curd).",
     prices:{small: 200, medium: 300, large: 500},
-    image: "images/biko.png",
+    image: "../images/biko.png",
     category: "desserts"
     },{
         name: "Suman ",
     desc:"Sticky rice cake wrapped in banana leaves (Tam-is or Bud-bud).",
     prices:{piece: 7},
-    image: "images/suman.png",
+    image: "../images/suman.png",
     category: "desserts"
     }
 
